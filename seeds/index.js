fb1260034e0f29ba380da0ff0b27ee64607c7c5f
Sean Ryan <sfryan95@gmail.com>
2024-02-24 15:41:22 -0800

    Update Session Config

--- seeds/index.js
+++ seeds/index.js
@@ -9,7 +9,7 @@ const citiesArr = require('./cities'); // Import array of cities with geolocatio
 const { descriptions } = require('./seedHelpers'); // Import descriptions helper
 
 // Database connection
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-aqua';
+const dbUrl = process.env.DB_URL; /*|| 'mongodb://localhost:27017/yelp-aqua'*/
 console.log(dbUrl);
 // Connect to the MongoDB database
 mongoose.connect(dbUrl, {
